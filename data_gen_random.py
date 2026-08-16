99510a7f95280caaa5eb3efc56611f2f60cefc6c
Edwin Calderon <caldeeh@gmail.com>
2026-08-15 19:10:43 -0500

    Se ha generado un histograma

--- data_gen_random.py
+++ data_gen_random.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 mu = 3
@@ -5,3 +6,14 @@ sigma = 0.5
 n = 2500
 vals = np.random.normal(loc=mu, scale=sigma, size=n)
 print(vals)
+
+# 1. Crear el histograma con 30 barras
+plt.hist(vals, bins=30, edgecolor="black", color="skyblue", density=True)
+
+# 2. Agregar títulos y etiquetas a los ejes
+plt.title("Distribución Normal (Campana de Gauss)")
+plt.xlabel("Valores")
+plt.ylabel("Densidad de probabilidad")
+
+# 3. Mostrar el gráfico en pantalla
+plt.show()
